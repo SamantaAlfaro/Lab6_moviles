3dbd43968070714027e76e9c71a5a67101405b10
KalaPinaki <leobago15@gmail.com>
2021-06-25 00:34:53 -0600

    pass photo camera

--- app/src/main/java/com/example/lab6/MainActivity.kt
+++ app/src/main/java/com/example/lab6/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.lab6
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import java.io.ByteArrayOutputStream
+
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var btnCapture: ImageButton = findViewById(R.id.btn_camera)
+
+        btnCapture!!.setOnClickListener {
+            val i = Intent(this, image_options::class.java)
+            startActivity(i)
+        }
+
     }
+
+
 }
