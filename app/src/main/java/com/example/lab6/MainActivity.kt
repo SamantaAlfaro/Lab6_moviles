5e17952915e104a92aa971ff7eae514597136f8a
SamantaAlfaro <samantasoul2612@gmail.com>
2021-06-25 00:16:27 -0600

    login by fingerprint scanner success

--- app/src/main/java/com/example/lab6/MainActivity.kt
+++ app/src/main/java/com/example/lab6/MainActivity.kt
@@ -1,11 +1,62 @@
 package com.example.lab6
 
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.hardware.biometrics.BiometricManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.ImageView
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
+
+    var imgCapture: ImageView? = null
+    val Image_Capture_Code = 1
+    val sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val cInt = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(cInt, Image_Capture_Code)
+
+        fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+            super.onActivityResult(requestCode, resultCode, data)
+            if (requestCode == Image_Capture_Code) {
+                if (resultCode == RESULT_OK) {
+                    val bp = data.extras!!["data"] as Bitmap?
+                    imgCapture!!.setImageBitmap(bp)
+                } else if (resultCode == RESULT_CANCELED) {
+                    Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+
     }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        TODO("Not yet implemented")
+    }
+
+
 }
---
+++ app/src/main/java/com/example/lab6/login_activity.kt
@@ -0,0 +1,81 @@
+package com.example.lab6
+
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.hardware.biometrics.BiometricManager
+import android.hardware.biometrics.BiometricPrompt
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CancellationSignal
+import android.widget.Button
+import android.widget.Toast
+
+class login_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+        var Auth_btn : Button = findViewById<Button>(R.id.Auth_btn)
+
+        Auth_btn.setOnClickListener{
+            showBiometricPrompt()
+        }
+
+    }
+
+
+    fun showBiometricPrompt(){
+        //fist, check if the device support Biometric prompt API
+        if(packageManager.hasSystemFeature(PackageManager.FEATURE_FINGERPRINT)){
+            val prompt = BiometricPrompt.Builder(this)
+                .setTitle("Autentciación")
+                .setDescription("Usa tu huella para ingresar a la app")
+                .setNegativeButton("Cancelar", mainExecutor, DialogInterface.OnClickListener { _, _ ->  })
+                .build()
+
+            prompt.authenticate(getCancellationSignal(), mainExecutor,
+                object: BiometricPrompt.AuthenticationCallback(){
+                    override fun onAuthenticationSucceeded(
+                        result: BiometricPrompt.AuthenticationResult?) {
+                        super.onAuthenticationSucceeded(result)
+                        //Action success display here
+                        fingerPrintAction()
+                    }
+
+                    override fun onAuthenticationFailed() {
+                        super.onAuthenticationFailed()
+                        badFingerPrintAction()
+                    }
+                }
+
+            )
+        }
+    }
+
+    fun fingerPrintAction(){
+        Toast.makeText(this, "Scanner de huella exitoso", Toast.LENGTH_LONG).show()
+        //val intent = Intent(this, MainActivity::class.java)
+        //startActivity(intent)
+    }
+    fun badFingerPrintAction(){
+        Toast.makeText(this, "Scanner de huella fallido", Toast.LENGTH_LONG).show()
+        //val intent = Intent(this, MainActivity::class.java)
+        //startActivity(intent)
+    }
+
+
+
+    fun getCancellationSignal(): CancellationSignal{
+        //with this signal, biometric prompt operations get cancel
+
+        val cancellationSignal= CancellationSignal()
+        cancellationSignal.setOnCancelListener {
+            //handle result action if you want here
+
+        }
+        return cancellationSignal
+    }
+
+}
